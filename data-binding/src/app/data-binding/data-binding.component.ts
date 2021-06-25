452f0466e221572ccdfa86a8188058fb2871d9ad
Raul Vinicius dos Santos Correia <60004704+feilonn@users.noreply.github.com>
2021-06-25 11:27:18 -0300

    Update

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -8,7 +8,27 @@ import { Component, OnInit } from '@angular/core';
 export class DataBindingComponent implements OnInit {
 
   url: string = 'http://google.com';
-  urlImagem: string = 'http://lorempixel.com/400/200/nature/'; 
+  urlImagem: string = 'http://lorempixel.com/400/200/nature/';
+  desativado: boolean = true;
+  valorAtual: any;
+  valorSalvo: any;
+  isMouseOver: boolean = false;
+
+  buttonClick(){
+    alert("Works!");
+  }
+
+  onKeyUp(event: KeyboardEvent){
+    this.valorAtual = ((<HTMLInputElement>event.target).value);
+  }
+
+  saveValue(value:any){
+    this.valorSalvo = value;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  }
 
   constructor() { }
 
